289bff3518f692d3852cbe0be3ad70f3e28c8c73
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-29 16:53:42 -0500

    Añadiendo las urls para modificar_destino y para eliminar_destino(aun no implementados)

--- destinos/turismo/urls.py
+++ destinos/turismo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.listar_destinos, name='listar'),
     path('añadir/', views.añadir_destino, name='añadir'),
+    path('modificar/<int:id>/', views.modificar_destino, name='modificar'),
+    path('eliminar/<int:id>/', views.eliminar_destino, name='eliminar'),
 ]

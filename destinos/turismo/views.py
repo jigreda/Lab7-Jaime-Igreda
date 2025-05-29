5806a7168e89c2a09e57b86f581c1436b4d0ac4b
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-29 15:39:12 -0500

    Creando las vistas para listar_destinos y añadir_destinos

--- destinos/turismo/views.py
+++ destinos/turismo/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import DestinosTuristicos
+from .forms import DestinoForm
+
+def listar_destinos(request):
+    destinos = DestinosTuristicos.objects.all()
+    return render(request, 'turismo/listar.html', {'destinos': destinos})
+
+def añadir_destino(request):
+    if request.method == 'POST':
+        form = DestinoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    else:
+        form = DestinoForm()
+    return render(request, 'turismo/añadir.html', {'form': form})

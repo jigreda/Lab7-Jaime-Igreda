23898d60fb6242a0d64e15f9516cb8d623c886d1
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-29 15:51:43 -0500

    Añadiendo la url para añadir_destino

--- destinos/turismo/urls.py
+++ destinos/turismo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.listar_destinos, name='listar'),
+    path('añadir/', views.añadir_destino, name='añadir'),
 ]

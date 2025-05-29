feeefa541090f81bfee9c96f63cc92babef4be59
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-28 22:11:01 -0500

    Creando el modelo DestinosTuristicos

--- destinos/turismo/models.py
+++ destinos/turismo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='imagenes/')
+    precioTour = models.DecimalField(max_digits=10, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nombreCiudad
 
-# Create your models here.

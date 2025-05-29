2e3731ac5da45016410b4123c741a76a5b6b9465
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-29 17:17:58 -0500

    Añadiendo las vistas para modificar_destino, eliminar_destino (para que funcione la primera version del proyecto que permite solo añadir destinos y mostrarlos)

--- destinos/turismo/views.py
+++ destinos/turismo/views.py
@@ -16,3 +16,21 @@ def añadir_destino(request):
     else:
         form = DestinoForm()
     return render(request, 'turismo/añadir.html', {'form': form})
+
+def modificar_destino(request, id):
+    destino = get_object_or_404(DestinosTuristicos, pk=id)
+    if request.method == 'POST':
+        form = DestinoForm(request.POST, request.FILES, instance=destino)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    else:
+        form = DestinoForm(instance=destino)
+    return render(request, 'turismo/modificar.html', {'form': form})
+
+def eliminar_destino(request, id):
+    destino = get_object_or_404(DestinosTuristicos, pk=id)
+    if request.method == 'POST':
+        destino.delete()
+        return redirect('listar')
+    return render(request, 'turismo/eliminar.html', {'destino': destino})

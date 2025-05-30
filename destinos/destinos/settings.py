c1023550639c8a5aaf2cfbef058d4cbc1ed67919
Jaime Igreda <jigreda@unsa.edu.pe>
2025-05-29 19:20:46 -0500

    Añadiendo el styles.css y configurando settings.py para que todo funciones correctamente

--- destinos/destinos/settings.py
+++ destinos/destinos/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,6 +30,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Application definition
 
